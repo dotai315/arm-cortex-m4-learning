746962fad5d76c38f34995a876aecbc4d8ff8abe
Đỗ Hữu Tài <57071897+dotai315@users.noreply.github.com>
2022-02-08 15:46:15 +0700

    Add files via upload

---
+++ gpio_input/build/main.d
@@ -0,0 +1,11 @@
+build/main.o: src/main.c inc/stm32f4xx.h inc/stm32f411xe.h inc/core_cm4.h \
+ inc/cmsis_version.h inc/cmsis_compiler.h inc/cmsis_gcc.h inc/mpu_armv7.h \
+ inc/system_stm32f4xx.h
+inc/stm32f4xx.h:
+inc/stm32f411xe.h:
+inc/core_cm4.h:
+inc/cmsis_version.h:
+inc/cmsis_compiler.h:
+inc/cmsis_gcc.h:
+inc/mpu_armv7.h:
+inc/system_stm32f4xx.h:
---
+++ gpio_input/src/main.c
@@ -0,0 +1,71 @@
+#include "stm32f4xx.h"
+#include <stdbool.h>
+
+#define PLL_P	2
+#define PLL_M	4
+#define PLL_N	100
+
+static void SystemClock_Config(void);
+static void GPIO_Config(void);
+static bool Button_Pressed(void);
+static void Toggle_Led(void);
+int main(void)
+{
+	SystemClock_Config();
+	GPIO_Config();
+	while (1) {
+    		if(Button_Pressed()) {
+			Toggle_Led();
+		}
+	}
+   	return 0;
+}
+
+static void SystemClock_Config(void)
+{
+	/* Turn on Clock Source */
+	RCC->CR |= RCC_CR_HSEON;
+	while (!(RCC->CR & RCC_CR_HSERDY));
+
+	/* Setting Power and Flash */
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+	PWR->CR |= PWR_CR_VOS_0 | PWR_CR_VOS_1;
+
+	FLASH->ACR |= FLASH_ACR_DCEN | FLASH_ACR_ICEN | FLASH_ACR_PRFTEN | FLASH_ACR_LATENCY_3WS;
+	
+	/* PLL Config and setting source clock for PLL */
+	RCC->PLLCFGR |= (PLL_N << RCC_PLLCFGR_PLLN_Pos) | (PLL_M << RCC_PLLCFGR_PLLM_Pos) | (PLL_P << RCC_PLLCFGR_PLLP_Pos);
+	RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE;
+
+	/* Config AHB, APB1 and APB2 */
+	RCC->CFGR |= RCC_CFGR_HPRE_DIV1 | RCC_CFGR_PPRE1_DIV2 | RCC_CFGR_PPRE2_DIV1;
+
+	/* Turn on PLL */
+	RCC->CR |= RCC_CR_PLLON;
+	while (!(RCC->CR & RCC_CR_PLLRDY));
+
+	/* Config System Clock  */
+	RCC->CFGR |= RCC_CFGR_SW_PLL;
+	while ((RCC->CFGR & RCC_CFGR_SWS_PLL) != RCC_CFGR_SWS_PLL);
+}
+
+static void GPIO_Config(void)
+{
+	/* GPIOA Clock Enable */
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN; 
+	/* Config GPIOA Pin 5 as ouput */
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+    /* Config GPIO C Pin 13 as input */
+    GPIOC->PUPDR |= GPIO_PUPDR_PUPD13_0; /* Pull up */
+}
+
+static bool Button_Pressed(void)
+{
+	return !((GPIOC->IDR & GPIO_IDR_ID13) == GPIO_IDR_ID13);
+}
+
+static void Toggle_Led(void)
+{
+	uint32_t odr = GPIOA->ODR;
+	GPIOA->BSRR |= ((odr & (1 << 5)) << 16u) | (~odr & (1 << 5));
+}
